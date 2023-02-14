bb2961280ee07769bf4da6aa4052c4fb5c892416
Laurens256 <duinlaurens2@gmail.com>
2023-02-14 22:27:39 +0100

    initial commit

---
+++ script.ts
@@ -0,0 +1,15 @@
+const ranges: NodeListOf<HTMLInputElement> =
+	document.querySelectorAll('input[type=range]')!;
+const head = document.querySelector('head')!;
+const body = document.querySelector('body')!;
+
+ranges.forEach((range) => {
+	range.addEventListener('input', () => {
+		range.setAttribute('data-value', range.value);
+
+		if (head) {
+			head.setAttribute(`data-value-${range.id}`, range.value);
+		}
+	});
+	range.dispatchEvent(new Event('input'));
+});
